53769e3c6976d4b149a46250d2fbe27882d06cc3
Akshay Gohil <akshay.gohil@bookmyshow.com>
2017-08-23 12:17:42 +0530

    Added koa router

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,10 @@
 import Koa from 'koa'
+import Router from 'koa-router'
 import path from 'path'
 import fs from 'fs'
 
 const app = new Koa()
-
-var readFileThunk = function(src) {
-    console.log('readFileThunk was called')
-    return new Promise(function(resolve, reject) {
-        fs.readFile(src, { 'encoding': 'utf8' }, function(err, data) {
-            if (err) return reject(err)
-            resolve(data)
-        })
-    })
-}
+const router = new Router()
 
 app.use(async function(ctx, next) {
     console.log("1")
@@ -20,16 +12,24 @@ app.use(async function(ctx, next) {
     console.log("2")
 })
 
-app.use(async function(ctx) {
+router.get('/', async function(ctx, next) {
     ctx.type = 'text/html'
-    let fsResponse = fs.readFileSync(path.resolve(__dirname,'../client/index.html'), { 'encoding': 'utf8' }, function(err, data) {
-
-    	console.log('Data : ', data)
-
+    let fsResponse = fs.readFileSync(path.resolve(__dirname,'../client/views/index.html'), { 'encoding': 'utf8' }, function(err, data) {
         if (err) return reject(err)
         else return data
     })
     ctx.body = fsResponse
 })
+
+router.get('/about', async function(ctx, next) {
+    ctx.type = 'text/html'
+    let fsResponse = fs.readFileSync(path.resolve(__dirname,'../client/views/about.html'), { 'encoding': 'utf8' }, function(err, data) {
+        if (err) return reject(err)
+        else return data
+    })
+    ctx.body = fsResponse
+})
+
+app.use(router.routes())
 
 app.listen(3000, () => (console.log('Server is listening on port 3000, goto http://localhost:3000 ')))
